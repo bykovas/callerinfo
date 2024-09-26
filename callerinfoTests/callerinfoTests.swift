fc729fe83a29177f4bc004e5a7752014807157fe
Denisas Bykovas <bykovas@icloud.com>
2024-09-26 14:33:19 +0300

    Initial Commit

---
+++ callerinfoTests/callerinfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  callerinfoTests.swift
+//  callerinfoTests
+//
+//  Created by Denisas Bykovas on 26/09/2024.
+//
+
+import Testing
+@testable import callerinfo
+
+struct callerinfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ callerinfoUITests/callerinfoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  callerinfoUITestsLaunchTests.swift
+//  callerinfoUITests
+//
+//  Created by Denisas Bykovas on 26/09/2024.
+//
+
+import XCTest
+
+final class callerinfoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
